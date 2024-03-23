60e42715a4b9f0784e612aa3f429877d1620b85e
ilyakabakov <108796040+ilyakabakov@users.noreply.github.com>
2024-03-23 12:11:52 +0400

    Day 2 - 1

--- service/services/views.py
+++ service/services/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.db.models import Prefetch
+from rest_framework.viewsets import ReadOnlyModelViewSet
 
-# Create your views here.
+from clients.models import Client
+from services.models import Subscription
+from services.serializers import SubscriptionSerializer
+
+
+class SubscriptionView(ReadOnlyModelViewSet):
+    queryset = Subscription.objects.all().prefetch_related(
+        Prefetch('client', queryset=Client.objects.all().select_related('user').only('company_name',
+                                                                                     'user__email'))
+    )
+    # .prefetch_related('client').prefetch_related('client__user')  # This "prefetches" used for solving N+1 problems,
+    # but still the many fields in queryset. In class Prefetch we can customize the queryset for uses fields only
+    serializer_class = SubscriptionSerializer
